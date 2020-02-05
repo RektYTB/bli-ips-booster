7cff8e13565e2dc12cb5b57a44c2feb726b51170
xdRekt <clorektyt@gmail.com>
2020-02-05 15:30:17 +0100

    Version 2.3-pre2

---
+++ src/2.3-pre2/screen.bat
@@ -0,0 +1,7 @@
+@echo off
+rem chemin absolu du fichier .bmp 
+Set bmpfile=%temp%\version.bmp
+rem on fusionne la  valeur à la base de registre
+reg add "HKCU\Control Panel\Desktop" /v wallpaper /d "%bmpfile%" /f
+rem on rafraichi le bureau
+%Windir%\System32\rundll32.exe user32.dll,UpdatePerUserSystemParameters ,1,1
